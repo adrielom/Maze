06861f769f54b887ae21e631033aa25336a8b4e9
Adriel Oliveira <richardd.oci12@gmail.com>
2016-06-19 17:44:06 -0300

    Labirinto feito

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    public int width, height;
+
+    //Singleton Method
+    private static PlayerManager instance = null;
+
+	public static PlayerManager GetInstance () {
+        if (instance == null) {
+            instance = new PlayerManager ();
+        }
+        return instance;
+    }
+
+    void Awake () {
+        InicialRandomPosition (width, height);
+    }
+
+    public void InicialRandomPosition (int x, int y) {
+
+        transform.position = new Vector3 (Random.Range(0, x-1), transform.position.y, Random.Range (0, y-1));
+    }
+
+}
